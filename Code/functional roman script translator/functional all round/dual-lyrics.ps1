3bb2b0ea20c7ceef00e731e0939acbc72d283968
owendrescher <owendrescher@gmail.com>
2026-04-25 15:06:53 -0400

    MLB Overlay update

---
+++ Code/functional roman script translator/functional all round/dual-lyrics.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+param(
+    [int]$Port = 8974,
+    [string]$TargetLanguage = "en",
+    [ValidateSet("googleweb", "none", "libretranslate")]
+    [string]$TranslationProvider = "googleweb"
+)
+
+$projectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+& (Join-Path $projectRoot "dual-lyrics-server.ps1") `
+    -Port $Port `
+    -TargetLanguage $TargetLanguage `
+    -TranslationProvider $TranslationProvider `
+    -OpenBrowser
